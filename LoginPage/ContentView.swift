9572361ba91f46423e0d5722aa645a5b7c64087a
Arturo Burigo <burigoarturo3@gmail.com>
2022-12-15 22:20:56 -0500

    Test

--- LoginPage/ContentView.swift
+++ LoginPage/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var ToggleOn = true
+
     var body: some View {
         VStack {
-            
+            Spacer()
             Text("Login")
                 .font(.largeTitle)
                 .foregroundColor(.orange)
             InputStyle(content: "Email")
+            PasswordField(content: "Password")
+            Button(action:{}){
+                Text("SignIn")
+                    .foregroundColor(.black)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.orange)
+                    .cornerRadius(8)
+            }
+            Spacer()
+            Toggle(isOn: $ToggleOn, label: {
+                HStack {
+                    Image(systemName: "faceid")
+                        .font(.system(size: 24))
+                    Text("Face ID")
+                        .font(.system(size: 24))
+                }.foregroundColor(.blue)
+            }).frame(width: 200)
+            
         }
         .foregroundColor(.white)
         .padding()
@@ -30,6 +50,21 @@ struct InputStyle: View {
     var body: some View {
         ZStack {
             TextField(content, text: $username)
+                .padding()
+                .background(.gray)
+                .cornerRadius(8)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+        }
+    }
+}
+
+struct PasswordField: View {
+    @State var password: String = ""
+    var content : String
+    var body: some View {
+        ZStack {
+            SecureField(content, text: $password)
                 .padding()
                 .background(.gray)
                 .cornerRadius(8)
@@ -56,7 +91,7 @@ struct InputStyle: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.light)
+            .preferredColorScheme(.dark)
         
     }
 }
